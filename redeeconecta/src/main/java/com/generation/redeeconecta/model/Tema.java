bf74ce7ff22bcb1f341c4836dcc849658eb27a16
lucas vinicius <lucas.thuran@hotmail.com>
2023-04-17 10:33:30 -0300

    Model 'tema'

---
+++ redeeconecta/src/main/java/com/generation/redeeconecta/model/Tema.java
@@ -0,0 +1,37 @@
+package com.generation.redeeconecta.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O Atributo Descrição é obrigatório")
+    private String descricao;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
